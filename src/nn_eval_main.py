a0b761ca9734f7692ba8532f7fc4ded3709cd3fa
Traffic Tse <traffictse@gmail.com>
2025-03-08 13:01:39 +0200

    Save current progress before switching branches

--- src/nn_eval_main.py
+++ src/nn_eval_main.py
@@ -1,4 +1,5 @@
 """Code to possibly evaluate the NN training approach. Currently, this only compares our and the CNN compilation. """
+
 import warnings
 from typing import List
 
@@ -13,10 +14,10 @@ from src.nn.brute_force_data import get_best_cnots
 from src.utils import random_hscx_circuit, tableau_from_circuit
 
 # Suppress all overflow warnings globally
-np.seterr(over='ignore')
+np.seterr(over="ignore")
 
 # Suppress FutureWarning
-warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action="ignore", category=FutureWarning)
 
 
 def collect_circuit_data(circuit: Circuit) -> dict:
@@ -27,10 +28,11 @@ def collect_circuit_data(circuit: Circuit) -> dict:
         "h": ops.get("h", 0),
         "s": ops.get("s", 0),
         "cx": ops.get("cx", 0),
-        "depth": circuit.to_qiskit().depth()
+        "depth": circuit.to_qiskit().depth(),
     }
 
 
+# "number of repetitions", "repetition index"???
 def our_compilation(circuit: Circuit, topology: Topology, n_rep: int):
     """
     Compilation from previous paper as a baseline.
@@ -44,7 +46,11 @@ def our_compilation(circuit: Circuit, topology: Topology, n_rep: int):
     clifford_tableau = tableau_from_circuit(clifford_tableau, circuit)
 
     circ_out = synthesize_tableau_perm_row_col(clifford_tableau, topology)
-    return {"n_rep": n_rep} | collect_circuit_data(circ_out) | {"method": "normal_heuristic"}
+    return (
+        {"n_rep": n_rep}
+        | collect_circuit_data(circ_out)
+        | {"method": "normal_heuristic"}
+    )
 
 
 def random_compilation(circuit: Circuit, topology: Topology, n_rep: int):
@@ -58,14 +64,20 @@ def random_compilation(circuit: Circuit, topology: Topology, n_rep: int):
     clifford_tableau = CliffordTableau(circuit.n_qubits)
     clifford_tableau = tableau_from_circuit(clifford_tableau, circuit)
 
-    def pick_pivot_callback(G, remaining: "CliffordTableau", remaining_rows: List[int], choice_fn=min):
+    def pick_pivot_callback(
+        G, remaining: "CliffordTableau", remaining_rows: List[int], choice_fn=min
+    ):
         row = np.random.choice(remaining_rows)
         col = row
         return row, col
 
-    circ_out = synthesize_tableau_perm_row_col(clifford_tableau, topology, pick_pivot_callback=pick_pivot_callback)
+    circ_out = synthesize_tableau_perm_row_col(
+        clifford_tableau, topology, pick_pivot_callback=pick_pivot_callback
+    )
     return {"n_rep": n_rep} | collect_circuit_data(circ_out) | {"method": "random"}
 
+
+# Bruteforce compilation, of course it is the optimal...
 def optimal_compilation(circuit: Circuit, topology: Topology, n_rep: int):
     """
     Brute force compilation of the circuit (may be slow for >=4 qubits!)
@@ -77,16 +89,22 @@ def optimal_compilation(circuit: Circuit, topology: Topology, n_rep: int):
     clifford_tableau = CliffordTableau(circuit.n_qubits)
     clifford_tableau = tableau_from_circuit(clifford_tableau, circuit)
 
-    best_permutation, score = get_best_cnots(clifford_tableau.inverse().inverse(), topology)[0]
+    best_permutation, score = get_best_cnots(
+        clifford_tableau.inverse().inverse(), topology
+    )[0]
+    print(f"best_permutation: {best_permutation}")
     best_permutation = iter(best_permutation)
 
-    def pick_pivot_callback(G, remaining: "CliffordTableau", remaining_rows: List[int], choice_fn=min):
+    def pick_pivot_callback(
+        G, remaining: "CliffordTableau", remaining_rows: List[int], choice_fn=min
+    ):
         row, col = next(best_permutation)
         return row, col
 
-    circ_out = synthesize_tableau_perm_row_col(clifford_tableau, topology, pick_pivot_callback=pick_pivot_callback)
+    circ_out = synthesize_tableau_perm_row_col(
+        clifford_tableau, topology, pick_pivot_callback=pick_pivot_callback
+    )
     return {"n_rep": n_rep} | collect_circuit_data(circ_out) | {"method": "optimum"}
-
 
 
 def main(n_qubits: int = 4, nr_gates: int = 1000):
@@ -97,7 +115,9 @@ def main(n_qubits: int = 4, nr_gates: int = 1000):
     :return:
     """
 
-    df = pd.DataFrame(columns=["n_rep", "num_qubits", "method", "h", "s", "cx", "depth"])
+    df = pd.DataFrame(
+        columns=["n_rep", "num_qubits", "method", "h", "s", "cx", "depth"]
+    )
     topo = Topology.complete(n_qubits)
     for i in range(20):
         print(i)
@@ -115,6 +135,7 @@ def main(n_qubits: int = 4, nr_gates: int = 1000):
         print("Random", df_dictionary["cx"])
 
     df.to_csv("test_clifford_synthesis.csv", index=False)
+    # Question: what should be the comparision metric? Mean, median, std, mse, etc.?
     print(df.groupby("method").mean())
 
 
